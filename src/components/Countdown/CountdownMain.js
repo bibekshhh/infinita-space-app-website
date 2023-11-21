08d442f8a0efc02540efb445ae7f6f9f6314c5c3
bibekshhh <90471455+bibekshhh@users.noreply.github.com>
2023-11-21 12:09:55 +0545

    Update CountdownMain.js

--- src/components/Countdown/CountdownMain.js
+++ src/components/Countdown/CountdownMain.js
@@ -1,19 +1,22 @@
 import CountdownTimer from "./CountdownTimer";
-import "./countdown.css"
+import "./countdown.css";
 
 const CountDownComponent = () => {
-    const SEVEN_DAYS_IN_MS = 13.417 * 24 * 60 * 60 * 1000;
-    const september23 = new Date(new Date().getFullYear(), 8, 23, 14, 0, 0, 0);
-    const NOW_IN_MS = september23.getTime();
-    console.log(NOW_IN_MS);
+    // Calculate the milliseconds for seven days
+    const SEVEN_DAYS_IN_MS = 7 * 24 * 60 * 60 * 1000;
 
+    // Set the target date for November 20th at 14:00:00
+    const november20 = new Date(new Date().getFullYear(), 10, 20, 14, 0, 0, 0);
+    const NOW_IN_MS = november20.getTime();
+
+    // Calculate the target date after seven days
     const dateTimeAfterSevenDays = NOW_IN_MS + SEVEN_DAYS_IN_MS;
 
     return (
         <div className="countdown-content-box">
-        <CountdownTimer targetDate={dateTimeAfterSevenDays} />
+            <CountdownTimer targetDate={dateTimeAfterSevenDays} />
         </div>
     );
-}
+};
 
 export default CountDownComponent;
